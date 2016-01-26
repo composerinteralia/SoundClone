a1b0f583eec13a717044162e19c0a58b00f3b0b1
Daniel Colson <danieljamescolson@gmail.com>
2016-01-26 17:08:55 -0500

    Ready for heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
+
 colson = User.create!(
   username: 'composerinteralia',
   password: 'password'
