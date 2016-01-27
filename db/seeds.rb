25880ea31a884477a3d8e63a287ee372e725c700
Daniel Colson <danieljamescolson@gmail.com>
2016-01-26 22:29:03 -0500

    Update guest account and add track seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@ colson = User.create!(
   password: 'password'
 )
 
+guest = User.create!(
+  username: 'secret-guest-account',
+  password: 'password'
+)
+
 mozart = User.create!(
   username: 'Wolfgang-Amadeus',
   password: 'password'
@@ -26,4 +31,27 @@ again = Track.create!(
   user: colson,
   title: "again",
   description: "Music for soprano, flute, clarinet, violin, and cello"
+)
+
+broken_consortini = Track.create!(
+  user: colson,
+  title: "broken consortini",
+  description: "A sextet written for Cygnus Ensemble"
+)
+
+eine_kleine = Track.create!(
+  user: mozart,
+  title: "eine kleine nacht musik"
+)
+
+guest_track1 = Track.create!(
+  user: guest,
+  title: "My first track!",
+  description: "Just a little something I put together one rainy evening"
+)
+
+guest_track2 = Track.create!(
+  user: guest,
+  title: "Exordium",
+  dexcription: "My first composition for orchestra!"
 )
