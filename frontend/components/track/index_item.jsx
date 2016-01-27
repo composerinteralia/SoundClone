d9212b1bb7bc79250ca3cc15ac0e54e0beaf56f0
Daniel Colson <danieljamescolson@gmail.com>
2016-01-27 17:46:01 -0500

    Implement track deletion

--- frontend/components/track/index_item.jsx
+++ frontend/components/track/index_item.jsx
@@ -1,27 +1,57 @@
 var React = require('react'),
+    ApiUtil = require('../../util/api_util'),
     ModalActions = require('../../actions/modal_actions'),
     UpdateTrack = require('./update');
 
 module.exports = React.createClass({
+  // this is definitely not the way to do this...
+
+  getInitialState: function () {
+    return ({ deleting: false });
+  },
+
   render: function () {
     var track = this.props.track;
+
+    var deleteClass = "popup";
+    if (this.state.deleting) {
+      deleteClass = "popup active";
+    }
+
     return (
       <li className="user-track">
         {track.title}
-        <button onClick={this._updateTrack}>Edit</button>
-        <button onClick={this._deleteTrack}>Delete</button>
+        <button onClick={this._update}>Edit</button>
+        <button onClick={this._delete}>Delete</button>
+        <div className={deleteClass}>
+          Are you sure you want to permanently delete this track?
+          <button onClick={this._cancelDelete}>Cancel</button>
+          <button onClick={this._reallyDelete}>Delete</button>
+          <div className="popup-arrow"></div>
+        </div>
       </li>
     );
   },
 
-  _updateTrack: function (e) {
+  _update: function (e) {
     e.preventDefault();
     var modal = <UpdateTrack trackId={this.props.track.id} />;
     ModalActions.receiveModal(modal);
   },
 
-  _deleteTrack: function () {
+  _delete: function (e) {
+    e.preventDefault();
+    this.setState({ deleting: true });
+  },
 
+  _cancelDelete: function (e) {
+    e.preventDefault();
+    this.setState({ deleting: false });
+  },
+
+  _reallyDelete: function (e) {
+    e.preventDefault();
+    ApiUtil.destroyTrack(this.props.track.id);
   }
 
 });
