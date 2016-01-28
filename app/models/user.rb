d223a2b09bd558b624cf3796f2cc5f79c877e0f3
= <=>
2016-01-28 10:07:28 -0500

    Add profile image to User using paperclip and AWS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   attr_reader :password
+  after_initialize :ensure_session_token
+
+  has_attached_file :profile_image, default_url: "sheep-logo.png"
+  validates_attachment_content_type :profile_image,
+                                    content_type: /\Aimage\/.*\Z/
+
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
-
-  after_initialize :ensure_session_token
 
   has_many :tracks
 
