11f4c5f2cb2407ae99e023cfa371987902965ad0
Daniel Colson <danieljamescolson@gmail.com>
2016-01-28 10:48:20 -0500

    Let sheep run away

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -6,7 +6,9 @@ module.exports = React.createClass({
     return (
       <header className="nav">
         <nav className="navbar group">
-          <div className="logo"></div>
+          <div className="logo">
+            <div className="sheep-logo"></div>
+          </div>
           <a href="#" className="logout" onClick={this._logout}>Sign Out</a>
         </nav>
       </header>
