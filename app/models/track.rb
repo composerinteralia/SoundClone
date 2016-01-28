ab7310c4b3768b8fe0b1d12558f1a31eeb8c9f4f
Daniel Colson <danieljamescolson@gmail.com>
2016-01-28 11:19:18 -0500

    Add audio attachment to Track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,6 +3,11 @@ class Track < ActiveRecord::Base
   validates_attachment_content_type :track_art,
                                     content_type: /\Aimage\/.*\Z/
 
+  # Later no default url - just validate presence, and only allow certain types of audio
+  has_attached_file :audio, default_url: "audio/cpe_solfeggio.mp3"
+  validates_attachment_content_type :track_art,
+                                    content_type: /\Aaudio\/.*\Z/
+
   validates :user, :title, presence: true
   validates :title, uniqueness: { scope: :user_id }
 
