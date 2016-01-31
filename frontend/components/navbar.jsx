2e646ab494e97e108898b7e0281d55058a184d08
Daniel Colson <danieljamescolson@gmail.com>
2016-01-30 22:04:15 -0500

    Add explore and profile links to navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,21 +1,31 @@
 var React = require('react'),
     Link = require('react-router').Link,
     History = require('react-router').History,
-    SessionsApiUtil = require('../util/sessions_api_util');
+    SessionsApiUtil = require('../util/sessions_api_util'),
+    CurrentUserStore = require('../stores/current_user');
 
 module.exports = React.createClass({
   mixins: [History],
 
   render: function () {
+    var currentUser = CurrentUserStore.currentUser()
+
     return (
       <header className="navbar">
         <nav className="group">
-          <figure className="logo">
+          <Link to="/" className="logo">
             <div className="sheep"></div>
             <div className="sheep clone"></div>
-          </figure>
+          </Link>
+          <Link to="/" className="explore">Explore</Link>
 
-          <a href="#" className="logout" onClick={this._logout}>Sign Out</a>
+          <div className="navbar-right group">
+            <Link to={"/users/" + currentUser.id} className="current-user-profile">
+              <img className="navbar-thumb" src={currentUser.profile_image_url}/>
+              {currentUser.username}
+            </Link>
+            <a className="logout" href="#" onClick={this._logout}>Sign Out</a>
+          </div>
         </nav>
       </header>
     );
